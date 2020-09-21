b593677a9656c2e458e8c81955a0c236d947326e
Jona M <jonameleqi@gmail.com>
2020-09-21 08:33:54 +0200

    php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include 'db.php'?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Dischi</title>
+</head>
+<body>
+    <div class="cds-container container">
+    <?php foreach($database as $data){?>
+        <div class="cd">
+            <img src="<?php echo $data['poster']; ?>" alt="poster">
+            <h3>
+            <?php echo $data['title']; ?>
+            </h3>
+            <span class="author">
+            <?php echo $data['author']; ?>
+            </span>
+            <span class="year">
+            <?php echo $data['year']; ?>
+            </span>
+        </div>
+    <?php } ?>  
+    </div>
+</body>
+</html>
